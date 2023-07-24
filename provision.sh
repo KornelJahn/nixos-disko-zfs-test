c298f6532a2e0b1c60dc6e1c92d2c8d25f45d08c
Kornel Jahn <kjahn.public@gmail.com>
2023-07-24 17:48:21 +0200

    provision.sh: Add nix-command feature.

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-sudo nix run github:nix-community/disko -- \
+sudo nix run github:nix-community/disko \
+  --extra-experimental-features nix-command \
+  -- \
   --mode disko ./disko-zfs-config.nix \
   --arg disks '[ "x" "y" ]' \
   --arg zpools '[ "rpool" ]'
