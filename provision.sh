6fdc488cfd7edec74262baf4ca7a7507049fba26
Kornel Jahn <kjahn.public@gmail.com>
2023-07-24 17:49:07 +0200

    provision.sh: Add flakes feature.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 sudo nix run github:nix-community/disko \
-  --extra-experimental-features nix-command \
+  --extra-experimental-features 'nix-command flakes' \
   -- \
   --mode disko ./disko-zfs-config.nix \
   --arg disks '[ "x" "y" ]' \
