813fa082fb439b97a228740c221e15514d2292de
Kornel Jahn <kjahn.public@gmail.com>
2023-07-26 12:59:29 +0200

    Re-format source.

--- hosts/qemu-guest.nix
+++ hosts/qemu-guest.nix
@@ -6,8 +6,13 @@
   boot = {
     initrd = {
       availableKernelModules = [
-        "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi"
-        "9p" "9pnet_virtio"
+        "virtio_net"
+        "virtio_pci"
+        "virtio_mmio"
+        "virtio_blk"
+        "virtio_scsi"
+        "9p"
+        "9pnet_virtio"
       ];
       kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
       postDeviceCommands = lib.mkIf (!config.boot.initrd.systemd.enable)
--- hosts/vbox-guest.nix
+++ hosts/vbox-guest.nix
@@ -5,7 +5,12 @@
 
 {
   boot.initrd.availableKernelModules = [
-    "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod"
+    "ata_piix"
+    "ohci_pci"
+    "ehci_pci"
+    "ahci"
+    "sd_mod"
+    "sr_mod"
   ];
 
   swapDevices = [ ];
